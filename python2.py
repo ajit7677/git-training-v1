ad67ddf7a85b82fd279d6edd2026cd2301c5d2d2
Ajit More <moreaju@gmail.com>
2023-07-26 00:50:32 +0530

    this is to remaing file load on remote repo

---
+++ python2.py
@@ -0,0 +1,4 @@
+print("statement2: we are commit some changes with my email_id")
+print('To improve the file for revert')
+
+print('python')
---
+++ python3.py
@@ -0,0 +1,3 @@
+print("statement3: we are commit some changes with my email_id")
+
+print('git test print statement')
