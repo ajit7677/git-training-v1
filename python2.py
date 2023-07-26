c4f8a31d4dcad817d43a325c5a6c7dbf7a4efca5
ajit7677 <96232529+ajit7677@users.noreply.github.com>
2023-07-26 10:47:12 +0530

    Update python2.py
    We are going to update this file for pull command

--- python2.py
+++ python2.py
@@ -1,4 +1,4 @@
 print("statement2: we are commit some changes with my email_id")
 print('To improve the file for revert')
-
+print("We are going to update this file for pull command")
 print('python')
