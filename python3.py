d8c3cf43c5c76530279f7f77b012db4efdcd5119
ajit7677 <96232529+ajit7677@users.noreply.github.com>
2023-07-26 10:49:57 +0530

    "We are going to update this file for the pull command"

--- python3.py
+++ python3.py
@@ -1,5 +1,5 @@
 print("statement3: we are commit some changes with my email_id")
-
+print("We are going to update this file for the pull command")
 print('git test print statement')
 
 print('We are made the changes in the file only for push command')
