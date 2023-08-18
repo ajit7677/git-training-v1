56b50e7e4c187e25ded3303af3672aea1277b506
Ajit More <moreaju@gmail.com>
2023-08-18 19:16:02 +0530

    we add new print statement in master branch for merge statement

--- python5.py
+++ python5.py
@@ -1,3 +1,4 @@
+## this is the master branch
 print("python new file i.e. python5")
-
 print("We are made the changes in the file only for the push command")
+print("We add new print statement in the master branch")
