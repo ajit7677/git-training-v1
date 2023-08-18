88f4b3d662fb7fc38b84bc868576fb813a59d571
Ajit More <moreaju@gmail.com>
2023-08-18 19:14:15 +0530

    we add new print statment for the merge statement

--- python5.py
+++ python5.py
@@ -1,3 +1,4 @@
 print("python new file i.e. python5")
 print("We are made the changes in the file only for the push command")
 print("we are added this comment only for newly created branch")
+print("we add new print statement on our branch")
